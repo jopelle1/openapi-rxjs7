1e56c058e9bb7bafcf463d6a983cba6e1ee9378c
Joseph Pellegrini <joseph@m19.com>
2022-01-18 17:04:22 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { DefaultApi } from 'api-client';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'openapi-rxjs7';
+
+  constructor(private defaultApi: DefaultApi) {
+    this.defaultApi.usersGet().subscribe();
+  }
 }
